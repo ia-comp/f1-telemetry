6cc59b70ff637daa393172ebc3b71ac8b06a82f1
I_A <none>
2026-01-24 19:36:40 +1100

    feat: can use dropdown lists to load events for a given year

--- frontend/src/pages/SessionResults.jsx
+++ frontend/src/pages/SessionResults.jsx
@@ -5,12 +5,49 @@ const API_URL = 'http://localhost:8000/api';
 
 function SessionResults() {
     const [sessionResults, setSessionResults] = useState([]);
+    const [yearSchedule, setYearSchedule] = useState([]);
+
+    const [year, setYear] = useState(2025);
+    const [gp, setGP] = useState("");
+
+    // const [availableSessions, setAvailableSessions] = useState([]);
+    // const [session, setSession] = useState("");
+
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    // for testing 
-    const year = 2023;
-    const gp = "monaco";
+
+    const fetchYearSchedule = async (selectedYear) => {
+      setYear(selectedYear)
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(`${API_URL}/session_results/${year}`);
+        if (!response.ok) throw new Error('Failed to fetch year schedule');
+        const data = await response.json();
+        setYearSchedule(data);
+      } catch (e) {
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    // const fetchEventSessions = async (selectedSession) => {
+    //   setSession(selectedSession)
+    //   setLoading(true);
+    //   setError(null);
+    //   try {
+    //     const response = await fetch(`${API_URL}/session_results/${year}/${gp}/${session}`);
+    //     if (!response.ok) throw new Error('Failed to fetch year schedule');
+    //     const data = await response.json();
+    //     setAvailableSessions(data);
+    //   } catch (e) {
+    //     setError(e.message);
+    //   } finally {
+    //     setLoading(false);
+    //   }
+    // };
 
     const fetchSessionResults = async () => {
       setLoading(true);
@@ -29,9 +66,45 @@ function SessionResults() {
   return (
     <div>
         <h1 className="text-4xl font-bold text-gray-50 mb-8 text-center">
-          🏎️ F1 Session Results
+          Session Results
         </h1>
-        
+
+        {/* Select year dropdown */}
+        <select 
+          defaultValue="Select year" 
+          className="select"
+          value={year}
+          onChange={(e) => fetchYearSchedule(Number(e.target.value))}
+        >
+          <option disabled={true}>Select year</option>
+          <option>2025</option>
+          <option>2024</option>
+          <option>2023</option>
+          <option>2022</option>
+          <option>2021</option>
+          <option>2020</option>
+          <option>2019</option>
+          <option>2018</option>
+        </select>
+
+        {/* Select race event dropdown */}
+        <select 
+          defaultValue="Select event" 
+          className="select"
+          value={gp}
+          onChange={(e) => setGP(e.target.value)}
+        >
+          <option disabled={true}>Select event</option>
+            {yearSchedule.map((event) => (
+              <option key={event.name} value={event.name}>
+                {event.round} - {event.name}
+              </option>
+            ))}
+        </select>
+
+        {/* Select sessions */}
+
+
         {sessionResults.length === 0 && !loading && !error && (
           <div className="bg-white rounded-lg shadow-md p-12 text-center text-gray-500">
             Select a race and click "Load Results" to view qualifying data
